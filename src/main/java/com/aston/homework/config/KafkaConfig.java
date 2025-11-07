379a628a15ac91861d96a5db04b6acddeb46a2f4
Dmitry Sheluto <shelutods@gmail.com>
2025-11-07 14:03:01 +0300

    add kafka producer test

--- src/main/java/com/aston/homework/config/KafkaConfig.java
+++ src/main/java/com/aston/homework/config/KafkaConfig.java
@@ -4,6 +4,7 @@ import com.aston.homework.dto.EventDto;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,29 +17,16 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
+    private String bootstrapServers;
 
     @Bean
     public ProducerFactory<String, EventDto> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-
-        // Aдреса серверов Kafka
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        // Класс для сериализации КЛЮЧА сообщения
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        // Класс для сериализации ЗНАЧЕНИЯ сообщения
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        // Настройка подтверждения получения сообщения:
-        // "all" - ждать подтверждения от ВСЕХ реплик (наиболее надежно)
-        // "1" - ждать подтверждения только от лидера
-        // "0" - не ждать подтверждения (самый быстрый, но ненадежный)
         configProps.put(ProducerConfig.ACKS_CONFIG, "all");
-        // Количество повторных попыток при временных ошибках сети
-        configProps.put(ProducerConfig.RETRIES_CONFIG, 3);
-        // Размер буфера для батчинга (группировки) сообщений в байтах
-        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-        // Таймаут для батчинга - максимальное время ожидания перед отправкой батча
-        configProps.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
---
+++ src/test/java/com/aston/homework/service/impl/KafkaProducerServiceTest.java
@@ -0,0 +1,81 @@
+package com.aston.homework.service.impl;
+
+import com.aston.homework.dto.EventDto;
+import com.aston.homework.dto.EventName;
+import com.aston.homework.dto.UserDtoOut;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class KafkaProducerServiceTest {
+    private ByteArrayOutputStream outputStream;
+
+    @Mock
+    private KafkaTemplate<String, EventDto> kafkaTemplate;
+
+    @InjectMocks
+    private KafkaProducerService kafkaProducerService;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    void shouldSendEventSuccess() {
+        //Given
+        EventName eventName = EventName.CREATE;
+        UserDtoOut userDtoOut = new UserDtoOut(1, "testName", "test@mail.com", 25, LocalDateTime.now());
+        CompletableFuture<SendResult<String, EventDto>> future = CompletableFuture.completedFuture(mock(SendResult.class));
+
+        when(kafkaTemplate.send(eq("user_events_topic"), any(EventDto.class))).thenReturn(future);
+
+        // When
+        kafkaProducerService.sendEvent(eventName, userDtoOut);
+
+        // Then
+        String output = outputStream.toString();
+        assertTrue(output.contains("event successfully sent to kafka"));
+        verify(kafkaTemplate).send(eq("user_events_topic"), any(EventDto.class));
+    }
+
+    @Test
+    void shouldSendEventFailed() {
+        //Given
+        EventName eventName = EventName.CREATE;
+        UserDtoOut userDtoOut = new UserDtoOut(1, "testName", "test@mail.com", 25, LocalDateTime.now());
+        CompletableFuture<SendResult<String, EventDto>> future = CompletableFuture.failedFuture(new RuntimeException());
+
+        when(kafkaTemplate.send(eq("user_events_topic"), any(EventDto.class))).thenReturn(future);
+
+        // When
+        kafkaProducerService.sendEvent(eventName, userDtoOut);
+
+        // Then
+        String output = outputStream.toString();
+        assertTrue(output.contains("event not delivered to kafka"));
+        verify(kafkaTemplate).send(eq("user_events_topic"), any(EventDto.class));
+
+    }
+}
